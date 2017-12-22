5b507974b9bd66fa6a2b584afffa2b1d3e5d31bd
Calum <100598217@student.swin.edu.au>
2017-12-22 17:27:22 +1100

    Initial commit

---
+++ app/src/main/java/com/calaxar/bluetoothled/MainActivity.java
@@ -0,0 +1,44 @@
+package com.calaxar.bluetoothled;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_send:
+                    mTextMessage.setText(R.string.title_send);
+                    return true;
+                case R.id.navigation_saved:
+                    mTextMessage.setText(R.string.fill_saved);
+                    return true;
+                case R.id.navigation_sequence:
+                    mTextMessage.setText(R.string.fill_sequence);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+}
